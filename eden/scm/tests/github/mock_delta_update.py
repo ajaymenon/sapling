49269af1b4588e01ccd8475b503b70ecf3158589
Ajay Menon <ajay.menon@xaira.com>
2026-06-12 10:48:43 -0700

    Add GitHub PR update delta comments

---
+++ eden/scm/tests/github/mock_delta_update.py
@@ -0,0 +1,134 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
+import os
+
+from sapling import error, extensions
+from sapling.ext.github import github_gh_cli, submit
+from sapling.ext.github.consts import query
+from sapling.ext.github.mock_utils import (
+    mock_run_git_command,
+    OWNER,
+    OWNER_ID,
+    REPO_ID,
+    REPO_NAME,
+    USER_NAME,
+)
+from sapling.result import Ok
+
+
+def _env(name: str) -> str:
+    value = os.environ.get(name)
+    if not value:
+        raise error.Abort(f"missing {name}")
+    return value
+
+
+async def _make_request(
+    real_make_request,
+    params,
+    hostname,
+    endpoint="graphql",
+    method=None,
+):
+    request_query = params.get("query")
+    if request_query == query.GRAPHQL_GET_REPOSITORY:
+        return Ok(
+            {
+                "data": {
+                    "repository": {
+                        "id": REPO_ID,
+                        "owner": {"id": OWNER_ID, "login": OWNER},
+                        "name": REPO_NAME,
+                        "isFork": False,
+                        "defaultBranchRef": {"name": "main"},
+                        "parent": None,
+                    }
+                }
+            }
+        )
+
+    if request_query == query.GRAPHQL_GET_PULL_REQUEST:
+        number = params["number"]
+        head_oid = _env(f"SL_TEST_PR{number}_HEAD")
+        title = "one" if number == 42 else "two"
+        return Ok(
+            {
+                "data": {
+                    "repository": {
+                        "pullRequest": {
+                            "id": f"PR_id_{number}",
+                            "url": f"https://github.com/{OWNER}/{REPO_NAME}/pull/{number}",
+                            "state": "OPEN",
+                            "headRefOid": head_oid,
+                            "headRefName": f"pr{number}",
+                            "baseRefOid": "base",
+                            "baseRefName": "main",
+                            "body": "",
+                            "title": title,
+                        }
+                    }
+                }
+            }
+        )
+
+    if request_query == query.GRAPHQL_UPDATE_PULL_REQUEST:
+        return Ok(
+            {
+                "data": {
+                    "updatePullRequest": {
+                        "pullRequest": {"id": params["pullRequestId"]}
+                    }
+                }
+            }
+        )
+
+    if request_query == query.GRAPHQL_GET_LOGIN:
+        return Ok({"data": {"viewer": {"login": USER_NAME}}})
+
+    if request_query == query.GRAPHQL_MERGE_BRANCH:
+        return Ok(
+            {
+                "data": {
+                    "mergeBranch": {
+                        "mergeCommit": {"oid": f"merge-{params['head'][:12]}"}
+                    }
+                }
+            }
+        )
+
+    if request_query == query.GRAPHQL_ADD_COMMENT:
+        expected_pr = _env("SL_TEST_DELTA_PR")
+        old_head = _env("SL_TEST_DELTA_OLD")
+        new_head = _env("SL_TEST_DELTA_NEW")
+        expected_subject = f"PR_id_{expected_pr}"
+        if params["subjectId"] != expected_subject:
+            raise error.Abort(
+                f"expected delta comment on {expected_subject}, got {params['subjectId']}"
+            )
+        body = params["body"]
+        expected_compare = (
+            f"https://github.com/{OWNER}/{REPO_NAME}/compare/{old_head}..{new_head}"
+        )
+        for expected in [
+            f"sapling-pr-delta old={old_head} new={new_head}",
+            expected_compare,
+        ]:
+            if expected not in body:
+                raise error.Abort(f"missing {expected!r} from {body!r}")
+        return Ok(
+            {
+                "data": {
+                    "addComment": {"commentEdge": {"node": {"id": "delta_comment"}}}
+                }
+            }
+        )
+
+    raise error.Abort(f"unexpected GitHub request: {params}")
+
+
+def uisetup(ui):
+    extensions.wrapfunction(github_gh_cli, "_make_request", _make_request)
+    extensions.wrapfunction(submit, "run_git_command", mock_run_git_command)
